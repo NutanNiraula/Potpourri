d908d868824328d8fb557bcb1cd687751e14b0e5
Nutan <nutanniraula@gmail.com>
2022-06-10 21:28:25 +0700

    iOS app init

---
+++ Potpourri/Potpourri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Potpourri
+//
+//  Created by Nutan Niraula on 10/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
